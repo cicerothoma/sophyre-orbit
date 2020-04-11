57c4c04be9ef8bfcc8f16c91ef59011b2eeca2c8
Collins Thomas <thomascollins582@gmail.com>
2020-04-11 15:03:42 +0100

    Working On File Upload

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage'
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  galleryCollection;
+  
+  constructor(private afStorage: AngularFireStorage, private afs: AngularFirestore) { }
 
+  
   ngOnInit(): void {
   }
+
+  
+    upload(event) {
+      const randomId = Math.random().toString(36).substring(2);
+      const file = event.target.files[0];
+      const filePath = `gallery/`;
+      const fileRef = this.afStorage.ref(filePath)
+      const task = this.afStorage.upload(filePath, file);
+
+       // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.afs.collection('gallery').add(this.downloadURL)
+         } )
+     )
+    .subscribe()
+    }
 
 }
